239a176877a5125d457a81746a88d82b2e6b4a80
allygreenberg8 <149001605+allygreenberg8@users.noreply.github.com>
2024-01-16 20:22:43 -0500

    Add files via upload

---
+++ tamid_calculator.py
@@ -0,0 +1,44 @@
+def main():
+    if __name__ == '__main__':
+        main()
+#welcome statement
+print("Welcome to the TAMID Calculator!")
+
+play = True #loop condition
+
+while play: #calc loop
+    start = str(input(("Would you like to calculate something (y/n): ")))
+    
+    if start == "y": #calculation 
+        operand_1 = float(input("Please enter the first number: "))
+        operand_2 = float(input("Please enter the second number: "))
+
+        function = str(input("Please enter your function: "))
+
+        #calculation conditions
+        if function == "+": #addition
+            result = (operand_1 + operand_2)
+
+        elif function == "-": #subtraction
+            result = (operand_1 - operand_2)
+
+        elif function == "*": #multiplication
+            result = (operand_1 * operand_2)
+
+        elif function == "/": #division
+            if operand_2 == 0: #division by 0 error condition
+                print("You cannot divide by 0!")
+                continue
+            result = (operand_1 / operand_2)
+
+        elif function == "%": #modulus
+            operand_1 = int(operand_1)
+            operand_2 = int(operand_2)
+            result = (operand_1 % operand_2)
+
+        print(f"The result is:  {str(result)}") #output statement
+
+    else: #exit conditions
+        print("Thanks for coming!")
+        play = False
+
